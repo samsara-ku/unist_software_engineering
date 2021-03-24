ce3ba9833ebf4d4d3ae6ae94008209ee334f9306
samsara-ku <gyh0316@gmail.com>
2021-03-24 23:15:57 +0900

    [feature/1] Some methods and pom.xml is updated #1
    Due to parsing problem, I make method about uniform input.
    
    Due to adding new methods, I add new dependency about StringUtils.

--- src/main/java/data/Load.java
+++ src/main/java/data/Load.java
@@ -1,12 +1,18 @@
 package data;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Load {
     private String[] categories;
     private String occupation;
 
     public Load(String args1, String args2) {
-        this.setCategories(args1.toLowerCase());
-        this.setOccupation(args2.toLowerCase());
+        this.setCategories(args1);
+        this.setOccupation(this.neutralizeString(args2));
+    }
+
+    public String neutralizeString (String input) {
+        return StringUtils.capitalize(input.toLowerCase());
     }
 
     public String[] parseCategory(String input) {
@@ -15,6 +21,14 @@ public class Load {
 
     public void setCategories(String categories) {
         this.categories = this.parseCategory(categories);
+
+        for (int i=0; i<this.categoriesLength(); i++) {
+            this.categories[i] = this.neutralizeString(this.categories[i]);
+        }
+    }
+
+    public int categoriesLength() {
+        return this.categories.length;
     }
 
     public String[] getCategories() {
