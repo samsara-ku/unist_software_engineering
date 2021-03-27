daf662c88c83feba0e50cb069e89307e8fdc5dcf
uni20181228 <myhjyy@unist.ac.kr>
2021-03-27 16:47:49 +0900

    Make the class that represents the values in dat file #2
    Make the 3 classes "Rating", "Movie", User".
    
    Each class have the method "SetValue()", that function set the values of the class.
    
    Make method "PrintStringList(ArrayList<String> StringList)" that prints the contents of the file.

--- src/main/java/LoadDat.java
+++ src/main/java/LoadDat.java
@@ -6,38 +6,116 @@ import java.io.FileNotFoundException;
 
 import java.util.*;
 
+class Rating {
+    //class of the one ratings of Users
+
+    public String InitialLine;
+    public int UserId;
+    public int MovieId;
+    public int Rating;
+    public int Timestamp;
+        
+    Rating(String InitialLine) {
+        this.InitialLine = InitialLine;
+    }
+
+    public void SetValue() {
+		//interpret the Initial input and set the value of the rating
+
+		String Line = InitialLine;
+		String[] ValueArr = InitialLine.split("::");
+		UserId = Integer.parseInt(ValueArr[0]);
+		MovieId = Integer.parseInt(ValueArr[1]);
+		Rating = Integer.parseInt(ValueArr[2]);
+		Timestamp = Integer.parseInt(ValueArr[3]);
+    }
+}
+
+class Movie {
+        //class of the movie's information
+
+    public String InitialLine;
+    public int MovieId;
+    public String Title;
+    public String[] Genres;
+
+    Movie(String InitialLine) {
+        this.InitialLine = InitialLine;
+    }
+
+    public void SetValue() {
+		//interpret the Initial input and set the info of movie
+
+		String Line = InitialLine;
+		String[] ValueArr = InitialLine.split("::");
+		MovieId = Integer.parseInt(ValueArr[0]);
+		Title = ValueArr[1];
+		Genres = ValueArr[2].split("|");
+    }
+}
+
+class User {
+        //class of the user's information
+
+    public String InitialLine;
+    public int UserId;
+    public String Gender;
+    public int Age;
+    public int Occupation;
+    public int Zip_code;
+	
+	User(String InitialLine) {
+        this.InitialLine = InitialLine;
+    }
+
+    public void SetValue() {
+		//interpret the Initial input and set the info of user
+
+		String Line = InitialLine;
+		String[] ValueArr = InitialLine.split("::");
+		UserId = Integer.parseInt(ValueArr[0]);
+		Gender = ValueArr[1];
+		Age = Integer.parseInt(ValueArr[2]);
+		Occupation = Integer.parseInt(ValueArr[3]);
+		Zip_code = Integer.parseInt(ValueArr[4]);
+    }
+}
+
 public class LoadDat {
 
     public static ArrayList<String> ReadDat(String FileName) {
-
-        //Read FileName.Dat file and return StringList
+        // Read FileName.Dat file and return StringList
 
         ArrayList<String> arrList = new ArrayList<String>();
         try{
-                File file = new File(String.format("../../../data/%s", FileName));
-                FileReader filereader = new FileReader(file);
-                BufferedReader bufReader = new BufferedReader(filereader);
-                String line = "";
-                while((line = bufReader.readLine()) != null){
-                arrList.add(line);
-                }
-                bufReader.close();
+            File file = new File(String.format("../../../data/%s", FileName));
+            FileReader filereader = new FileReader(file);
+            BufferedReader bufReader = new BufferedReader(filereader);
+            String line = "";
+            while((line = bufReader.readLine()) != null){
+            arrList.add(line);
+            }
+            bufReader.close();
         }catch (FileNotFoundException e) {
-                System.out.println(e);
+            System.out.println(e);
         }catch (IOException e) {
-                System.out.println(e);
+            System.out.println(e);
         }
         return arrList;
     }
 
-    public static void main(String[] args) {
+    public static void PrintStringList(ArrayList<String> StringList) {
+        // print and check that we actually read the file properly
 
+        for (int i=0; i<StringList.size(); i++) {
+            System.out.println(StringList.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
         //main function
 
-/*        ArrayList<String> StringList = LoadDat.ReadDat("users.dat");
-        for (int i=0; i<StringList.size(); i++) {
-                System.out.println(StringList.get(i));
-        } //for test
-        */
+        ArrayList<String> StringList = LoadDat.ReadDat("movies.dat");
+        LoadDat.PrintStringList(StringList);
     }
 }
