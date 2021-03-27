d27e0d4204543282beecd35191ccc2cc969fff19
samsara-ku <gyh0316@gmail.com>
2021-03-28 02:20:12 +0900

    [feature/1] main function is changed due to reading user input #1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,11 @@
+import data.Load;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class HelloWorld {
-    public static void main(String[] arg){
-        System.out.println("hi");
+    public static void main(String[] arg) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        Load Load = new Load(sc.nextLine(), sc.nextLine());
     }
 }
