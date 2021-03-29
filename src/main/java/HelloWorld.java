8bf66f0af4bcc7bebecaf13e09ad54a2032d2377
coffee-dori <kevin0653@unist.ac.kr>
2021-03-29 22:06:08 +0900

    [feature/6] Change the input format #6

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,8 @@
 import data.Load;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class HelloWorld {
-    public static void main(String[] arg) throws IOException {
-        Scanner sc = new Scanner(System.in);
-
-        Load Load = new Load(sc.nextLine(), sc.nextLine());
+    public static void main(String[] args) throws IOException {
+        Load Load = new Load(args[0], args[1]);
     }
 }
