90c6a2951651edb97a760eba87caa0fa9ce6c487
coffee-dori <kevin0653@unist.ac.kr>
2021-03-30 16:45:30 +0900

    [feature/6] Change if/else statement #6

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,11 +5,10 @@ public class HelloWorld {
     public static void main(String[] args) throws IOException {
 
         // Check whether the arguments are two or not
-        if(args.length == 2){
-            Load Load = new Load(args[0], args[1]);
-        }
-        else{
+        if(args.length != 2){
             System.out.println("Inappropriate # of arguments!");
+            return;
         }
+        Load Load = new Load(args[0], args[1]);
     }
 }
