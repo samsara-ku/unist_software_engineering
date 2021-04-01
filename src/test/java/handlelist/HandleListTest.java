113661a9cd8e521156a020c215ebed250d3999e3
Yoonyoung Jung <myhjyy@unist.ac.kr>
2021-04-02 00:39:27 +0900

    modulize testcases #10

--- src/test/java/handlelist/HandleListTest.java
+++ src/test/java/handlelist/HandleListTest.java
@@ -1,17 +1,57 @@
 package handlelist;
-
 import data.Load;
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 
 public class HandleListTest {
 
-  @Test
-  public void testLoad() {
-    Load test = new Load("comedy", "unemployed");
-    HandleList listHandler = new HandleList(test.getMovieList(), test.getUserList());
+		@Test
+		public void testLoad() {
+			Load actualInput = new Load("comedy|action", "unemployed");
 
-    System.out.println(listHandler.getAverageRating());
+			assertNotNull(actualInput.getCategories());
+			System.println("passed test_setCategories");
 
-  }
+			assertNotNull(actualInput.getOccupation());
+			System.println("passed test_setOccupation");
+
+			assertNotNull(actualInput.getMovieList());
+			System.println("passed test_setMovieList");
+
+			assertNotNull(actualInput.getUserList());
+			System.println("passed test_setUserList");
+
+			System.println("passed test_Load");
+		}
+
+		@Test
+		public void testparseCategory() {
+			String[] expectedResult = ["comedy", "action"];
+			String[] actualResult = new Load.parseCategory("comedy|action");
+			assertArrayEquals(expectedResult, actualResult);
+			System.println("passed test_parseCategory");
+		}
+
+		@Test
+		public void testCategories() {
+			Load actualInput = new Load("comedy|action", "unemployed");
+			assertArrayEquals(actualInput.getCategories(), ["comedy", "action"]);
+			System.println("passed test_getCategories");
+
+			int actualResult = 2;
+			assertEquals(actualInput.categoriesLength(), 2);
+			System.println("passed test_CategoriesLength");		
+		}
+
+		@Test
+		public void testOccupation() {
+			Load actualInput = new Load("comedy|action", "unemployed");
+			assertEquals("unemployed", actualInput.getOccupation);
+			System.println("passed test_getOccupation");
+		}
+
+    @Test
+    public void testLoad() {
+			Load expectedResults = new Load("comedy", "unemployed");
+		}
 }
