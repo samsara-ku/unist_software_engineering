d6c572406ff203b42a1caf1335044b3fa7fb48d9
koimg8545 <koimg8545@naver.com>
2021-04-02 01:02:11 +0900

    [feature/10] error handleing #10

--- src/test/java/handlelist/HandleListTest.java
+++ src/test/java/handlelist/HandleListTest.java
@@ -10,48 +10,45 @@ public class HandleListTest {
 			Load actualInput = new Load("comedy|action", "unemployed");
 
 			assertNotNull(actualInput.getCategories());
-			System.println("passed test_setCategories");
+			System.out.println("passed test_setCategories");
 
 			assertNotNull(actualInput.getOccupation());
-			System.println("passed test_setOccupation");
+			System.out.println("passed test_setOccupation");
 
 			assertNotNull(actualInput.getMovieList());
-			System.println("passed test_setMovieList");
+			System.out.println("passed test_setMovieList");
 
 			assertNotNull(actualInput.getUserList());
-			System.println("passed test_setUserList");
+			System.out.println("passed test_setUserList");
 
-			System.println("passed test_Load");
+			System.out.println("passed test_Load");
 		}
 
 		@Test
 		public void testparseCategory() {
-			String[] expectedResult = ["comedy", "action"];
-			String[] actualResult = new Load.parseCategory("comedy|action");
+			String[] expectedResult = {"comedy", "action"};
+			Load expected = new Load("comedy|action", "unemployed");
+			String[] actualResult = expected.parseCategory("comedy|action");
 			assertArrayEquals(expectedResult, actualResult);
-			System.println("passed test_parseCategory");
+			System.out.println("passed test_parseCategory");
 		}
 
 		@Test
 		public void testCategories() {
 			Load actualInput = new Load("comedy|action", "unemployed");
-			assertArrayEquals(actualInput.getCategories(), ["comedy", "action"]);
-			System.println("passed test_getCategories");
+			String[] expectedResult = {"comedy", "action"};
+			assertArrayEquals(actualInput.getCategories(), expectedResult);
+			System.out.println("passed test_getCategories");
 
 			int actualResult = 2;
 			assertEquals(actualInput.categoriesLength(), 2);
-			System.println("passed test_CategoriesLength");		
+			System.out.println("passed test_CategoriesLength");		
 		}
 
 		@Test
 		public void testOccupation() {
 			Load actualInput = new Load("comedy|action", "unemployed");
-			assertEquals("unemployed", actualInput.getOccupation);
-			System.println("passed test_getOccupation");
-		}
-
-    @Test
-    public void testLoad() {
-			Load expectedResults = new Load("comedy", "unemployed");
+			assertEquals("unemployed", actualInput.getOccupation());
+			System.out.println("passed test_getOccupation");
 		}
 }
