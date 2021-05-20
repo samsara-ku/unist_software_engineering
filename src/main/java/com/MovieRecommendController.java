38eab68a581067373b1b50b48330dbc01bce11af
koimg8545 <koimg8545@naver.com>
2021-05-20 13:20:36 +0900

    [feature/36] Correct error #36

--- src/main/java/com/MovieRecommendController.java
+++ src/main/java/com/MovieRecommendController.java
@@ -1,1 +1,49 @@
-package com;import com.fasterxml.jackson.core.JsonProcessingException;import com.fasterxml.jackson.databind.ObjectMapper;import factory.MovieRecommendFactory;import factory.link.LinkUserAndRating;import factory.rating.MovieTitleRecommend;import factory.user.LoadMovieTitle;import java.util.ArrayList;import java.util.HashMap;import java.util.Map;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RequestBody;import org.springframework.web.bind.annotation.RequestMapping;import org.springframework.web.bind.annotation.ResponseBody;import org.springframework.web.bind.annotation.RestController;@RestController@RequestMapping("movies")public class MovieRecommendController {  @ResponseBody  @GetMapping("/recommend")  public Object recommend(@RequestBody final MovieRecommend mediator) {    MovieRecommendFactory movieRecommendFactory = new MovieRecommendFactory(mediator.getTitle(), mediator.getLimit());    boolean titleIsValid = movieRecommendFactory.titleIsValid();    boolean limitIsValid = movieRecommendFactory.limitIsValid();    if(!titleIsValid || !limitIsValid){      return movieRecommendFactory.getErrorMessage();    }    ArrayList<String> object = new ArrayList<>();    movieRecommendFactory.getResult().forEach(i -> {      Map<String, String> temp = new HashMap<>();      temp.put("imdb", String.format("https://www.imdb.com/title/tt%s", i.split("/")[1]));      temp.put("genre", i.split("/")[2]);      temp.put("title", i.split("/")[0]);      ObjectMapper mapper = new ObjectMapper();      try {        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(temp);        object.add(json);      } catch (JsonProcessingException e) {        e.printStackTrace();      }    });    return object;  }}
+package com;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import factory.MovieRecommendFactory;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("movies")
+public class MovieRecommendController {
+
+  @ResponseBody
+  @GetMapping("/recommend")
+  public Object recommend(@RequestBody final MovieRecommend mediator) {
+    MovieRecommendFactory movieRecommendFactory = new MovieRecommendFactory(mediator.getTitle(), mediator.getLimit());
+    boolean titleIsValid = movieRecommendFactory.titleIsValid();
+    boolean limitIsValid = movieRecommendFactory.limitIsValid();
+
+    if(!titleIsValid || !limitIsValid){
+      return movieRecommendFactory.getErrorMessage();
+    }
+
+    ArrayList<String> object = new ArrayList<>();
+
+    movieRecommendFactory.getResult().forEach(i -> {
+      Map<String, String> temp = new HashMap<>();
+      temp.put("imdb", String.format("https://www.imdb.com/title/tt%s", i.split("/")[1]));
+      temp.put("genre", i.split("/")[2]);
+      temp.put("title", i.split("/")[0]);
+      ObjectMapper mapper = new ObjectMapper();
+      try {
+        String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(temp);
+        object.add(json);
+      } catch (JsonProcessingException e) {
+        e.printStackTrace();
+      }
+    });
+
+    return object;
+  }
+}
