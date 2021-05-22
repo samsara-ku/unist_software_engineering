eb478b6d21b19b0e40db47a3a3cfefadcb2fb4a9
coffee-dori <kevin0653@unist.ac.kr>
2021-05-22 17:09:40 +0900

    [feature/38] Fix AverageRatingTest due to renamed class #38

--- src/test/java/handlelist/AverageRatingTest.java
+++ src/test/java/handlelist/AverageRatingTest.java
@@ -3,7 +3,8 @@ package handlelist;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import data.Load;
+import factory.rating.RatingWithTwoArgs;
+import factory.user.LoadTwoArgs;
 import java.util.ArrayList;
 import org.junit.Test;
 
@@ -11,22 +12,22 @@ public class AverageRatingTest {
 
   @Test
   public void testListGetters() {
-    Load data = new Load("comedy", "unemployed");
-    AverageRating averageRating = new AverageRating(data.getMovieList(), data.getUserList());
-
+    LoadTwoArgs data = new LoadTwoArgs("comedy", "unemployed");
+    RatingWithTwoArgs averageRating =
+        new RatingWithTwoArgs(data.getMovieList(), data.getUserList());
 
     assertNotNull("userIdList is NULL", averageRating.getUserIdList());
     System.out.println("passed test getUserIdList");
 
     assertNotNull("movieIdList is NULL", averageRating.getMovieIdList());
     System.out.println("passed test getMovieIdList");
-
   }
 
   @Test
   public void testListSetters() {
-    Load data = new Load("comedy", "unemployed");
-    AverageRating averageRating = new AverageRating(data.getMovieList(), data.getUserList());
+    LoadTwoArgs data = new LoadTwoArgs("comedy", "unemployed");
+    RatingWithTwoArgs averageRating =
+        new RatingWithTwoArgs(data.getMovieList(), data.getUserList());
 
     averageRating.setIdLists();
 
@@ -35,7 +36,6 @@ public class AverageRatingTest {
 
     assertNotNull("movieIdList is NULL", averageRating.getMovieIdList());
     System.out.println("passed test setMovieIdList");
-
 
     ArrayList<String> userIdList = averageRating.getUserIdList();
     ArrayList<String> movieIdList = averageRating.getMovieIdList();
@@ -47,51 +47,44 @@ public class AverageRatingTest {
 
     assertTrue("movieList changed", movieIdList.equals(averageRating.getMovieIdList()));
     System.out.println("passed test repeat setMovieIdList");
-
   }
-
 
   @Test
   public void testGetAverageRating() {
-    Load data = new Load("comedy", "unemployed");
-    AverageRating handleList = new AverageRating(data.getMovieList(), data.getUserList());
+    LoadTwoArgs data = new LoadTwoArgs("comedy", "unemployed");
+    RatingWithTwoArgs handleList = new RatingWithTwoArgs(data.getMovieList(), data.getUserList());
     double averageRating = handleList.getAverageRating();
 
     assertNotNull("AverageRating is NULL", averageRating);
-    System.out
-        .println(String.format("The average rating is \"%.2f\"", averageRating));
+    System.out.println(String.format("The average rating is \"%.2f\"", averageRating));
   }
 
   @Test
   public void testSomeArgues() {
     double averageRating;
 
-    Load data = new Load("comedy", "unemployed");
-    AverageRating handleList = new AverageRating(data.getMovieList(), data.getUserList());
+    LoadTwoArgs data = new LoadTwoArgs("comedy", "unemployed");
+    RatingWithTwoArgs handleList = new RatingWithTwoArgs(data.getMovieList(), data.getUserList());
     averageRating = handleList.getAverageRating();
 
-    System.out
-        .println(String.format("The average rating is \"%.2f\"", averageRating));
+    System.out.println(String.format("The average rating is \"%.2f\"", averageRating));
 
-    data = new Load("comedy|romance", "artist");
-    handleList = new AverageRating(data.getMovieList(), data.getUserList());
+    data = new LoadTwoArgs("comedy|romance", "artist");
+    handleList = new RatingWithTwoArgs(data.getMovieList(), data.getUserList());
     averageRating = handleList.getAverageRating();
 
-    System.out
-        .println(String.format("The average rating is \"%.2f\"", averageRating));
+    System.out.println(String.format("The average rating is \"%.2f\"", averageRating));
 
-    data = new Load("comedy|romance|adventure", "lawyer");
-    handleList = new AverageRating(data.getMovieList(), data.getUserList());
+    data = new LoadTwoArgs("comedy|romance|adventure", "lawyer");
+    handleList = new RatingWithTwoArgs(data.getMovieList(), data.getUserList());
     averageRating = handleList.getAverageRating();
 
-    System.out
-        .println(String.format("The average rating is \"%.2f\"", averageRating));
+    System.out.println(String.format("The average rating is \"%.2f\"", averageRating));
 
-    data = new Load("comedy|romance|adventure|sci-fi|horror", "farmer");
-    handleList = new AverageRating(data.getMovieList(), data.getUserList());
+    data = new LoadTwoArgs("comedy|romance|adventure|sci-fi|horror", "farmer");
+    handleList = new RatingWithTwoArgs(data.getMovieList(), data.getUserList());
     averageRating = handleList.getAverageRating();
 
-    System.out
-        .println(String.format("The average rating is \"%.2f\"", averageRating));
+    System.out.println(String.format("The average rating is \"%.2f\"", averageRating));
   }
 }
