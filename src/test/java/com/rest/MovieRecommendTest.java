92aa0bfea05161c7a6b8375dd9f2efe22d5d871d
koimg8545 <koimg8545@naver.com>
2021-06-05 08:54:07 +0900

    [feature/47] Add csv files #47

--- src/test/java/com/rest/MovieRecommendTest.java
+++ src/test/java/com/rest/MovieRecommendTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
-public class MovieRecommendTest {
+public class MovieRecommendModelTest {
 
   @Test
   public void allTest() {
--- src/test/java/factory/MovieRecommendFactoryTest.java
+++ src/test/java/factory/MovieRecommendFactoryTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class MovieRecommendFactoryTest {
+public class MovieRecommendModelFactoryTest {
 
   @Test
   public void testTitleIsValid() {
