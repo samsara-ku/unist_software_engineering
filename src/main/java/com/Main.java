e1145e5d4a9a806deb792ea895e57e12881cf385
koimg8545 <koimg8545@naver.com>
2021-06-05 08:57:37 +0900

    [feature/47] Edit controller to use database #47

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,14 +1,28 @@
 package com;
 
+import com.csvtomongo.jobs.LinksToMongoJob;
+import com.csvtomongo.jobs.MoviesToMongoJob;
+import com.csvtomongo.jobs.PostersToMongoJob;
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Main {
+
+  @Autowired
+  private LinksToMongoJob linksToMongoJob;
+  @Autowired
+  private MoviesToMongoJob moviesToMongoJob;
+  @Autowired
+  private PostersToMongoJob postersToMongoJob;
+  @Autowired
+  private static MongoTemplate mongoTemplate;
 
   public static void main(String[] args) throws IOException {
     SpringApplication.run(Main.class, args);
   }
-
 }
--- src/main/java/com/rest/MovieRecommendController.java
+++ src/main/java/com/rest/MovieRecommendController.java
@@ -1,25 +1,50 @@
 package com.rest;
 
 
+import com.csvtomongo.model.Links;
+import com.csvtomongo.model.Movies;
+import com.csvtomongo.model.Posters;
+import com.csvtomongo.model.RecommendMovie;
+import com.csvtomongo.repository.LinkRepository;
+import com.csvtomongo.repository.MovieRecommendRepository;
+import com.csvtomongo.repository.PosterRepository;
 import factory.MovieRecommendFactory;
 import java.util.ArrayList;
 import java.util.HashMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieRecommendController {
 
-  @ResponseBody
-  @GetMapping("/movies/recommendations")
-  public Object recommend(@RequestBody(required = false) MovieRecommend mediator) {
+  private MovieRecommendRepository movieRepository;
+  private LinkRepository linkRepository;
+  private PosterRepository posterRepository;
+
+  public MovieRecommendController(MovieRecommendRepository movieRepository,
+      LinkRepository linkRepository,
+      PosterRepository posterRepository) {
+    this.movieRepository = movieRepository;
+    this.linkRepository = linkRepository;
+    this.posterRepository = posterRepository;
+  }
+
+  @Autowired
+  private MongoTemplate mongoTemplate;
+
+  @RequestMapping(value = "/movies/recommendations", method = RequestMethod.GET)
+  public Object recommend(@RequestParam("title") String title, @RequestParam("limit") String limit,
+      MovieRecommend mediator) {
     MovieRecommendFactory movieRecommendFactory = new MovieRecommendFactory(mediator.getTitle(),
         mediator.getLimit());
     boolean titleIsValid = movieRecommendFactory.titleIsValid();
     boolean limitIsValid = movieRecommendFactory.limitIsValid();
-    ArrayList<HashMap<String, String>> object = new ArrayList<>();
 
     if (!titleIsValid || !limitIsValid) {
       String error = movieRecommendFactory.getErrorMessage();
@@ -30,14 +55,37 @@ public class MovieRecommendController {
       return temp;
     }
 
-    movieRecommendFactory.getResult().forEach(i -> {
-      HashMap<String, String> temp = new HashMap<>();
-      temp.put("imdb", String.format("https://www.imdb.com/title/tt%s", i.split("/")[1]));
-      temp.put("genre", i.split("/")[2]);
-      temp.put("title", i.split("/")[0]);
-      object.add(temp);
-    });
+    return getMovieList(movieRecommendFactory.getResult());
+  }
 
+
+  public ArrayList<RecommendMovie> getMovieList(ArrayList<Integer> movieList){
+    ArrayList<RecommendMovie> object = new ArrayList<>();
+
+    movieRepository.deleteAll();
+    for(int i : movieList){
+      Movies movie = mongoTemplate.findOne(
+          Query.query(Criteria.where("id").is(i)), Movies.class);
+      Links link = mongoTemplate.findOne(
+          Query.query(Criteria.where("id").is(i)), Links.class);
+      Posters poster = mongoTemplate.findOne(
+          Query.query(Criteria.where("id").is(i)), Posters.class);
+
+      RecommendMovie recommendMovie = new RecommendMovie();
+      recommendMovie.setId(movie.getId());
+      recommendMovie.setGenre(movie.getGenre());
+      recommendMovie.setTitle(movie.getTitle());
+      recommendMovie.setLink(link.getLink());
+      if(poster == null){
+        recommendMovie.setPoster("https://www.shutterstock.com/image-vector/no-image-available-sign-internet-web-261719003");
+      }
+      else {
+        recommendMovie.setPoster(poster.getPoster());
+      }
+      movieRepository.save(recommendMovie);
+      object.add(recommendMovie);
+    }
     return object;
   }
+
 }
